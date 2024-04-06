8a790892d8374ecdfb66a98ac4a36f6dbd1ff728
Andres <ricardo.gomez.restrepo@gmail.com>
2024-04-05 21:00:47 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="mx-auto flex justify-center align-middle">
+        <h1>Hello World!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
